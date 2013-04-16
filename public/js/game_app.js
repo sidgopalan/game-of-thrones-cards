56f7bd2e0166529d2b1b037e86260ea3c577c755
Justin Zhang <twojustin@gmail.com>
2013-04-15 17:00:56 -0700

    list all games

--- public/js/game_app.js
+++ public/js/game_app.js
@@ -1,5 +1,11 @@
 var GameModel = Backbone.Model.extend({
-	urlRoot:'/game',
+	urlRoot:'/games',
+	idAttribute: 'name',
+});
+
+var GamesCollection = Backbone.Collection.extend({
+	url:'/games',
+	model:GameModel,
 });
 
 var NavView = Marionette.ItemView.extend({
@@ -16,4 +22,27 @@ var NavView = Marionette.ItemView.extend({
 	doCreateGame:function(){
 		console.log('create');
 	},
+});
+
+var GameView = Marionette.ItemView.extend({
+	template:'#game_template',
+	tagName:'tr',
+	ui:{
+		name:'span.name',
+	},
+	onRender:function(){
+		this.ui.name.html(this.model.get('name'));
+	}
+});
+
+var GamesView = Marionette.CompositeView.extend({
+	template: 'script#games_template',
+	tagName:'table',
+	className:'table',
+	itemView: GameView,
+	itemViewContainer:'tbody',
+
+	initialize:function(options){
+		console.log(options.collection);
+	}
 });
